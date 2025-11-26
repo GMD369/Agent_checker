188087467393b2141a2fb8a8c73b9d19ada86cd0
GMD369 <chdryghulam475@gmail.com>
2025-11-26 20:53:36 +0500

    add

--- check1.py
+++ check1.py
@@ -5,3 +5,7 @@ def is_string(value):
 # string length checker
 def is_string_length(value, length):
     return is_string(value) and len(value) == length
+
+# numeric checker
+def is_numeric(value):
+    return isinstance(value, (int, float))
