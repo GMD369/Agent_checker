aaf67f92feef8a71b2f00e0a6714ce610990b1e9
GMD369 <chdryghulam475@gmail.com>
2025-11-26 22:19:29 +0500

    update

--- check1.py
+++ check1.py
@@ -9,3 +9,10 @@ def is_string_length(value, length):
 # numeric checker
 def is_numeric(value):
     return isinstance(value, (int, float))
+
+# special characters
+def has_special_characters(value):
+    if not is_string(value):
+        return False
+    special_characters = "!@#$%^&*()-+?_=,<>/"
+    return any(char in special_characters for char in value)
