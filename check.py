99dea4e06f851f4ceaca471b7c5a7440678a0e45
GMD369 <chdryghulam475@gmail.com>
2025-12-17 19:51:34 +0500

    checkss

--- check.py
+++ check.py
@@ -4,3 +4,10 @@ def safe_divide(a, b):
         return a / b
     except ZeroDivisionError:
         return None
+
+# check list index out of range
+def safe_get(lst, index):
+    try:
+        return lst[index]
+    except IndexError:
+        return None
